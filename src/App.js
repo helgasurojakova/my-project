efb6a4e94e5e3ba6438a29452ea97daf1ab28dbc
Olga Suroyakova <o.suroyakova@sigma-it.ru>
2021-12-13 11:59:04 +0300

    Created counter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Counter from "./counter";
+import {useState} from "react";
 
 function App() {
+
+const [count,setCount] = useState(0)
+
+  const decrement = () => setCount(count -1)
+  const dis = count === 5
+
+
   return (
     <div className="App">
       <header className="App-header">
+        <button onClick={() => setCount(count + 1)}>increment</button>
+        <button disabled={dis} onClick={decrement}>decrement</button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,6 +28,9 @@ function App() {
         >
           Learn React
         </a>
+        {count}
+        <Counter/>
+
       </header>
     </div>
   );
---
+++ src/counter.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Counter = () => {
+    return (
+        <div>
+            olya
+        </div>
+    );
+};
+
+export default Counter;
