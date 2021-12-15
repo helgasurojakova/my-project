ca0fc87e4e92ebfd620ca1fc088441635623b863
Olga Suroyakova <o.suroyakova@sigma-it.ru>
2021-12-15 17:49:21 +0300

    ClassCounter and Counter

--- src/App.js
+++ src/App.js
@@ -1,51 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import Counter from "./counter";
-import {useState} from "react";
-import {Checkbox, HStack} from "@chakra-ui/react";
-import MaterialFile from "./materialFile";
+import React, {useState} from "react";
+import ClassCounter from "./components/ClassCounter";
 
 function App() {
-
-const [count,setCount] = useState(0)
-
-  const decrement = () => setCount(count -1)
-  const dis = count === 5
-
+    const [value, setValue] = useState('INPUT TEXT')
 
   return (
     <div className="App">
-      <header className="App-header">
-        <p><b>{process.env.REACT_APP_MY_ENV}</b>  WORLD!</p>
-        <button onClick={() => setCount(count + 1)}>increment</button>
-        <button disabled={dis} onClick={decrement}>decrement</button>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        {count}
-        <Counter/>
-        <HStack spacing={10} direction='row'>
-          <Checkbox size='sm' colorScheme='red'>
-            Checkbox
-          </Checkbox>
-          <Checkbox size='md' colorScheme='green' defaultIsChecked>
-            Checkbox
-          </Checkbox>
-          <Checkbox size='lg' colorScheme='orange' defaultIsChecked>
-            Checkbox
-          </Checkbox>
-        </HStack>
-        <MaterialFile/>
-      </header>
+        <ClassCounter/>
     </div>
   );
 }
