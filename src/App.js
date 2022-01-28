af52d1834e721a557926f94543498c899426acd3
Olga Suroyakova <o.suroyakova@sigma-it.ru>
2022-01-28 10:24:30 +0300

    Redirect to error, AppRouter

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React from "react";
 import '../src/styles/App.css'
-import About from "./pages/About";
-import Posts from "./pages/Posts";
 import Navbar from "./components/UI/Navbar/Navbar";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
+import AppRouter from "./components/AppRouter";
 
 function App() {
     return (
         <BrowserRouter>
             <Navbar/>
-            <Routes>
-                <Route path="/about" element={<About/>}/>
-                <Route path="/posts" element={<Posts/>}/>
-                <Route path={'*'} element={<Posts/>}/>
-            </Routes>
+            <AppRouter/>
         </BrowserRouter>
     )
 }
