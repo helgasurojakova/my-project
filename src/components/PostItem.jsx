f6d1b638fa77f0dddbc85f85544c12ab0e8a033e
Olga Suroyakova <o.suroyakova@sigma-it.ru>
2022-01-29 23:39:36 +0300

    Comments + JSON filter

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import MyButton from "./UI/button/MyButton";
+import {useNavigate} from 'react-router-dom';
 
 const PostItem = (props) => {
+    const navigate = useNavigate()
     return (
         <div className="post">
             <div className="post__content">
-                <strong>{props.post.id}. {props.post.title}</strong>
+                <strong>{props.post.id}.{props.post.title}</strong>
                 <div>
                     {props.post.body}
                 </div>
             </div>
             <div className="post__btns">
-                <MyButton onClick={() => props.remove(props.post)}>Удалить</MyButton>
+                <MyButton onClick={() => {
+                    navigate(`/posts/${props.post.id}`)
+                }}>
+                    Open
+                </MyButton>
+                <MyButton onClick={() => props.remove(props.post)}>
+                    Delete
+                </MyButton>
             </div>
         </div>
     );
